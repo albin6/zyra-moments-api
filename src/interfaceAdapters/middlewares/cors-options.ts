0cefc318af48c769abb010811f364bdc3a922a82
Albin Aji <albinpariyarathu@gmail.com>
2025-07-22 11:58:08 +0530

    modified:   src/interfaceAdapters/middlewares/cors-options.ts

--- src/interfaceAdapters/middlewares/cors-options.ts
+++ src/interfaceAdapters/middlewares/cors-options.ts
@@ -1,12 +1,13 @@
 import { config } from "../../shared/config";
 
 export const corsOptions = {
-  origin: [
-    config.cors.ALLOWED_ORIGIN_ONE,
-    config.cors.ALLOWED_ORIGIN_TWO,
-    config.cors.ALLOWED_ORIGIN_LOCAL_ONE,
-    config.cors.ALLOWED_ORIGIN_LOCAL_TWO,
-  ],
+  // origin: [
+  //   config.cors.ALLOWED_ORIGIN_ONE,
+  //   config.cors.ALLOWED_ORIGIN_TWO,
+  //   config.cors.ALLOWED_ORIGIN_LOCAL_ONE,
+  //   config.cors.ALLOWED_ORIGIN_LOCAL_TWO,
+  // ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Authorization", "Content-Type", "stripe-signature"],
   credentials: true,
